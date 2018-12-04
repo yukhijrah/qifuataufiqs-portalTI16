b2ca2d1c9ef66ec98e286f0c9292f9c8d8ba6154
Sukmo Afri Ardi Saputro <qifuataufiqs@gmail.com>
2018-12-04 11:20:44 +0700

    + entity mahasiswa

---
+++ app/src/main/java/qifuataufiqs/app/portalti16/entity/Mahasiswa.java
@@ -0,0 +1,14 @@
+package qifuataufiqs.app.portalti16.entity;
+
+public class Mahasiswa {
+    private String name;
+    private String nim;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+}
